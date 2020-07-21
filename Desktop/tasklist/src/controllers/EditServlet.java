83dbc3298afd806877e38593d934d93cc5251892
yuki.shiroma <0ea5v.30326377d@ezweb.ne.jp>
2020-07-22 02:44:03 +0900

    Add DestroyServlet.java

--- Desktop/tasklist/src/controllers/EditServlet.java
+++ Desktop/tasklist/src/controllers/EditServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
@@ -15,16 +14,16 @@ import models.Task;
 import utils.DAO;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class EditServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public EditServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -35,17 +34,19 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DAO.createEntityManager();
 
-        List<Task> tasks = em.createNamedQuery("getTask", Task.class)
-                                   .getResultList();
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+        // 該当のIDのメッセージ1件のみをデータベースから取得
+        Task m = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
-        request.setAttribute("tasks", tasks);
+        // メッセージ情報とセッションIDをリクエストスコープに登録
+        request.setAttribute("tasks", m);
+        request.setAttribute("_token", request.getSession().getId());
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        // メッセージIDをセッションスコープに登録
+        request.getSession().setAttribute("tasks_id", m.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
     }
-    }
-
-
+}
